b4d5ff0ab7d896b5e4bdc8d0d7624d6f1cb795ed
Halid Awol <halidm@ksu.edu>
2023-05-15 17:14:31 -0700

    existing code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import React from 'react';
+import {CardList} from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component';
+class App extends React.Component 
+{
+  constructor(){
+    super();
+    this.state = {
+      monsters:[],
+      searchField: ''
+    };
+  }
+  // feth the json file and then render the data and then in the json there is this array called 
+  //users so put the user as the value for monsters and using set state it will render it
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json()).then(users => this.setState({monsters:users}));
+  }
+render(){
+  const {monsters, searchField} = this.state;
+  const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchBox
+      placeholder='search monsters'
+      handleChange={e => this.setState({searchField: e.target.value})}/>
+      <CardList monsters = {filteredMonsters}/>
+  
+      
     </div>
   );
 }
-
+}
 export default App;
